2e512e177256be271ff59485c83153f188064ca3
Sa Sajjad <sqsa93103@gmail.com>
2023-07-02 11:37:51 +0600

    Snx-R: Lets go

--- snx/uploading_apps.sh
+++ snx/uploading_apps.sh
@@ -1,12 +1,12 @@
 cd ~/$rom_name
 cd out/target/product/$device/system/product/priv-app/SystemUI
-bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/uploading.sh) -f *.apk
+bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
 cd ..
 cd Settings
-bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/uploading.sh) -f *.apk
+bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
 cd ..
 cd ..
 cd ..
 cd framework
-bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/uploading.sh) -f *.apk
-bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/uploading.sh) -f framework.jar
+bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
+bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f framework.jar
