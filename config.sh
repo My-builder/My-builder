a98518edd773c97fafaffd89191d827f0d3275a6
Sa Sajjad <sqsa93103@gmail.com>
2023-07-05 14:03:39 +0600

    Snx-R: Initial test

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # add repo, dt, vt, kt etc...
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b Sa-Sajjad-patch-1 -g default,-mips,-darwin,-notdefault
 # replace with your manifest
-git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 3 .repo/local_manifests
+git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b 6 .repo/local_manifests
 # sync script
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
