13e6710ee228e4ca27e9f81d7099506fb4d3440f
Sa Sajjad <sqsa93103@gmail.com>
2023-07-24 18:50:28 +0600

    Gapps & Vanilla build test

--- snx/notify.sh
+++ snx/notify.sh
@@ -1,6 +1,6 @@
 curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=
 ◦•●◉✿ 𝔹𝕦𝕚𝕝𝕕 𝕊𝕥𝕒𝕣𝕥𝕖𝕕 ✿◉●•◦
-
+      
 Info~ <code>$rom_name-$CIRRUS_CHANGE_MESSAGE</code>
       
 Status~ <a href='https://cirrus-ci.com/build/$CIRRUS_BUILD_ID'>Click here</a>
