e55a5070c41d3f73c6873d1314508a491d6ec83a
Sa Sajjad <sqsa93103@gmail.com>
2023-08-02 19:47:42 +0600

    SystemUI: Statusbar clock background chip styles [2/2]

--- snx/upload_apps.sh
+++ snx/upload_apps.sh
@@ -1,9 +1,9 @@
 cd ~/$rom_name
 cd out/target/product/$device/system/product/priv-app/SystemUI
 bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
-# cd ..
-# cd Settings
-# bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
+cd ..
+cd Settings
+bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
 # cd ..
 # cd ..
 # cd ..
