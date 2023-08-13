6948d4cc59b2dc3734db52147d479ab579317a71
Sa Sajjad <sqsa93103@gmail.com>
2023-08-13 08:25:24 +0600

    Test dt fumk

--- snx/dt_sync.sh
+++
@@ -1,1 +0,0 @@
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b 1 .repo/local_manifests
--- snx/sync.sh
+++
@@ -1,5 +0,0 @@
-mkdir ~/$rom_name
-cd ~/$rom_name
-rm -rf .repo/local_manifests
-repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
