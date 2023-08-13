8990941ce40369e29e4d487e3f556084540311e1
Sa Sajjad <sqsa93103@gmail.com>
2023-08-13 07:27:02 +0600

    Test dt

--- config.sh
+++ config.sh
@@ -1,9 +1,7 @@
 # add repo, dt, vt, kt etc..
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b 10 -g default,-mips,-darwin,-notdefault
 # replace with your manifest
-# git clone https://github.com/My-builder/My-builder --depth 1 .repo/local_manifests
-wget https://github.com/My-builder/My-builder/blob/$CIRRUS_BRANCH/local_manifest.xml
-mv local_manifest.xml .repo/local_manifests
+git clone https://github.com/Sa-Sajjad/manifest.git --depth 1 -b hmp .repo/local_manifests
 # sync script
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
