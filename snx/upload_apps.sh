a2b19666574d0e88fd122387fb06c646beb529cc
Sa Sajjad <sqsa93103@gmail.com>
2023-08-14 22:07:28 +0600

    Next build
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/upload_apps.sh
+++ snx/upload_apps.sh
@@ -1,14 +1,14 @@
 cd ~/$rom_name
 cd out/target/product/$device/system/product/priv-app/SystemUI
 zip SystemUI.zip SystemUI.apk 
-bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.zip
+bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.zip
 cd ..
 cd Settings
 zip Settings.zip Settings.apk 
-bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.zip
+bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.zip
 # cd ..
 # cd ..
 # cd ..
 # cd framework
-# bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
-# bash <(curl -s https://raw.githubusercontent.com/My-builder/My-builder/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f framework.jar
+# bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
+# bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f framework.jar
