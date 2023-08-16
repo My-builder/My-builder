295ce09048ba0bfd066224663f4c19ba4a1f1ab2
Sa Sajjad <sqsa93103@gmail.com>
2023-08-16 16:24:14 +0600

    lavender: tedt buold
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # add repo, dt, vt, kt etc..
 repo init --depth=1 --no-repo-verify -u https://Sa-Sajjad:$ght@github.com/S-A-build/android_manifest_nusa.git -b client -g default,-mips,-darwin,-notdefault
 # replace with your manifest
-git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b snx2 .repo/local_manifests
+git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b eas .repo/local_manifests
 # sync script
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
