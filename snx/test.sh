63422aab59a3eb1684e1eff51ae0d10fb19fd644
Sa Sajjad <sqsa93103@gmail.com>
2023-08-16 22:51:46 +0600

    lavender: tedt build
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/test.sh
+++ snx/test.sh
@@ -1,7 +1,7 @@
 ZIP=$(echo *.zip)
 name=$(basename *.zip)
 curl -F document=@$ZIP "https://api.telegram.org/bot${bot_api}/sendDocument" \
--F chat_id="$tg_id" \
--F "disable_web_page_preview=true" \
--F "parse_mode=html" \
--F caption="<code>$name</code>"
+    -F chat_id="$tg_id" \
+    -F "disable_web_page_preview=true" \
+    -F "parse_mode=html" \
+    -F caption="<code>$name</code>"
