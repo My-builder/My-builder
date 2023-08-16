a055ab0a921545ee664a2b5951791b4372abc366
Sa Sajjad <sqsa93103@gmail.com>
2023-08-16 23:21:28 +0600

    Snx-R: Switch to tg uploader for apps upload
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # Snx-R
-fuk
-
-name=$(basename *.zip)
-ZIP=$(echo *.zip)
-curl -F document=@$ZIP "https://api.telegram.org/bot${bot_api}/sendDocument" \
--F chat_id="$tg_id" \
--F "disable_web_page_preview=true" \
--F "parse_mode=html" \
--F caption="<code>$name</code>"
--- snx/upload_apps.sh
+++ snx/upload_apps.sh
@@ -1,14 +1,15 @@
 cd ~/$rom_name
 cd out/target/product/$device/system/product/priv-app/SystemUI
 zip SystemUI.zip SystemUI.apk 
-bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.zip
+bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh)
 cd ..
 cd Settings
 zip Settings.zip Settings.apk 
-bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.zip
+bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh)
 # cd ..
 # cd ..
 # cd ..
 # cd framework
 # bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
 # bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f framework.jar
+    
