140f3faf07578ad37de8f35f1e7eb05bc01678d1
Sa Sajjad <sqsa93103@gmail.com>
2023-08-21 22:42:20 +0600

    try again
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/sync.sh
+++ snx/sync.sh
@@ -1,6 +1,7 @@
 mkdir ~/$rom_name
 cd ~/$rom_name
-rm -rf .repo/local_manifests
-command=$(head $CIRRUS_WORKING_DIR/config.sh -n $(expr $(grep 'build/envsetup.sh' $CIRRUS_WORKING_DIR/config.sh -n | cut -f1 -d:) - 1))
-only_sync=$(grep 'repo sync' $CIRRUS_WORKING_DIR/config.sh)
-bash -c "$command" || true
+wget https://sa.mirror-hub.workers.dev/0:/snx/Gapps%20Trash%20Mod.zip
+# rm -rf .repo/local_manifests
+# command=$(head $CIRRUS_WORKING_DIR/config.sh -n $(expr $(grep 'build/envsetup.sh' $CIRRUS_WORKING_DIR/config.sh -n | cut -f1 -d:) - 1))
+# only_sync=$(grep 'repo sync' $CIRRUS_WORKING_DIR/config.sh)
+# bash -c "$command" || true
---
+++ snx/test.sh
@@ -0,0 +1,12 @@
+curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=
+◦•●◉✿ 🅑🅤🅘🅛🅓 🅢🅤🅒🅒🅔🅔🅓 ✿◉●•◦
+      
+File Name: <code>snx</code>
+
+Total Size: $(du -sh *.zip | cut -d - -f 1 | cut -d / -f 1)
+
+Time Took: $((0 / 60)) minute(s) and $((0 % 60)) second(s).
+
+Download Link: <a href='https://devuploads.com/69'>Here</a>
+
+◦•●◉✿ by Sã Śâjjãd ✿◉●•◦" -d "chat_id=$tg_id" -d "parse_mode=HTML"
