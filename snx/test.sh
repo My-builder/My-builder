d29c316931c16302327e85f95f87a3ca21c3e7d5
Sa Sajjad <sqsa93103@gmail.com>
2023-08-21 22:55:38 +0600

    try again
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/test.sh
+++ snx/test.sh
@@ -1,4 +1,9 @@
 wget https://sa.mirror-hub.workers.dev/0:/snx/Corvus_v4.7-lavender-03052020-OFFICIAL-1437.zip
+
+START=$(date +"%s")
+END=$(date +"%s")
+DIFF=$(($END - $START))
+
 curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=
 ◦•●◉✿ 🅑🅤🅘🅛🅓 🅢🅤🅒🅒🅔🅔🅓 ✿◉●•◦
       
@@ -6,7 +11,7 @@ File Name: <code>snx</code>
 
 Total Size: $(du -sh *.zip | cut -d - -f 1 | cut -d / -f 1)
 
-Time Took: $((0 / 60)) minute(s) and $((0 % 60)) second(s).
+Time Took: $((($DIFF / 60)) minute(s) and $((($DIFF % 60)) second(s).
 
 Download Link: <a href='https://devuploads.com/69'>Here</a>
 
