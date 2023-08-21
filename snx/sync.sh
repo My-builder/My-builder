fe302ae7b9f2049885ab99b4c98a394dfebf537d
Sa Sajjad <sqsa93103@gmail.com>
2023-08-21 16:28:33 +0600

    try again
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/sync.sh
+++ snx/sync.sh
@@ -4,4 +4,3 @@ rm -rf .repo/local_manifests
 command=$(head $CIRRUS_WORKING_DIR/config.sh -n $(expr $(grep 'build/envsetup.sh' $CIRRUS_WORKING_DIR/config.sh -n | cut -f1 -d:) - 1))
 only_sync=$(grep 'repo sync' $CIRRUS_WORKING_DIR/config.sh)
 bash -c "$command" || true
-git clone --depth=1 https://gitlab.com/Project-Nexus/nexus-clang.git prebuilts/clang/host/linux-x86/nexus-clang -b nexus-14
