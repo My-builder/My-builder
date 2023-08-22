928b2d83cd38ae09386325327c63c67c6589866b
Sa Sajjad <sqsa93103@gmail.com>
2023-08-22 23:36:31 +0600

    battery test vmro
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/build.sh
+++ snx/build.sh
@@ -23,14 +23,14 @@ link=https://sourceforge.net/projects/snx-r/files/Rn7/$dlink/download
 curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=
 ◦•●◉✿ 🅑🅤🅘🅛🅓 🅢🅤🅒🅒🅔🅔🅓 ✿◉●•◦
       
-Name: <code>$dlink</code>
+➜Name: <code>$dlink</code>
 
-Size: $(du -sh *.zip | cut -d N -f 1)
+➜Size: $(du -sh $file | cut -d N -f 1)
 
-Time Took: $(($DIFF / 60)) Min $(($DIFF % 60)) Sec
+➜Time Took: $(($DIFF / 60)) Min $(($DIFF % 60)) Sec
 
-Download Link: <a href='$link'>Vanilla</a>
+➜Download Link: <a href='$link'>Vanilla</a>
 
-Total Disk used: $(du -sh ~/$rom_name/ | cut -d / -f 1)
+➜Total Disk used: $(du -sh ~/$rom_name/ | cut -d / -f 1)
 
 ◦•●◉✿ by Sã Śâjjãd ✿◉●•◦" -d "chat_id=$tg_id" -d "parse_mode=HTML"
