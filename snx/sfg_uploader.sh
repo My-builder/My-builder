fec8dc0ee2f35dab355fd1308516d2b0a5d90e98
Sa Sajjad <sqsa93103@gmail.com>
2023-08-22 10:29:23 +0600

    test builder
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/sfg_uploader.sh
+++ snx/sfg_uploader.sh
@@ -1,7 +1,7 @@
-set -e
-cd ~/$rom_name
 file=out/target/product/$device/*.zip
 dlink=$(basename out/target/product/$device/*.zip)
+# file2=out/target/product/$device/*Gapps*.zip
+# dlink2=$(basename out/target/product/$device/*Gapps*.zip)
 rsync -vhcP -e ssh $file sa-sajjad@frs.sourceforge.net:/home/frs/project/snx-r/Rn7/
 echo "Download link https://sourceforge.net/projects/snx-r/files/Rn7/$dlink/download"
 link=https://sourceforge.net/projects/snx-r/files/Rn7/$dlink/download
@@ -16,12 +16,8 @@ Size: $(du -sh *.zip | cut -d N -f 1)
 
 Time Took: $(($DIFF / 60)) Min $(($DIFF % 60)) Sec
 
-Download Link: <a href='$link'>Here</a>
+Download Link: <a href='$link'>Vanilla</a>
 
 Total Disk used: $(du -sh ~/$rom_name/ | cut -d / -f 1)
 
 ◦•●◉✿ by Sã Śâjjãd ✿◉●•◦" -d "chat_id=$tg_id" -d "parse_mode=HTML"
-rm -rf $file
-rm -rf $file
-rm -rf $file
-rm -rf $file
