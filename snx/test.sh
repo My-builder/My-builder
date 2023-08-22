ed24934690e4cfc0a36d1d1f93e0e6b830ebd2da
Sa Sajjad <sqsa93103@gmail.com>
2023-08-22 07:36:09 +0600

    try again
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/test.sh
+++ snx/test.sh
@@ -1,3 +1,5 @@
+mkdir ~/$rom_name
+cd ~/$rom_name
 START=$(date +"%s")
 wget https://sa.mirror-hub.workers.dev/0:/snx/Nusantara_eLTS-10-lavender-25092022-NubXD-1601.zip
 END=$(date +"%s")
@@ -10,7 +12,7 @@ File Name: <code>snx</code>
 
 File Size: $(du -sh *.zip | cut -d N -f 1)
 
-Time Took: $(($DIFF / 60))m:$(($DIFF % 60))s
+Time Took: $(($DIFF / 60)):$(($DIFF % 60))m
 
 Download Link: <a href='https://devuploads.com/69'>Here</a>
 
