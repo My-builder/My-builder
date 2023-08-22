d6ea54eb405f321231abeff2fea9cdb1e3e30fd6
Sa Sajjad <sqsa93103@gmail.com>
2023-08-22 07:28:55 +0600

    try again
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/test.sh
+++ snx/test.sh
@@ -14,6 +14,6 @@ Time Took: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s).
 
 Download Link: <a href='https://devuploads.com/69'>Here</a>
 
-File Size: $(du -sh /$rom_name/ | cut -d N -f 1)
+Total Size: $(du -sh /$rom_name/ | cut -d N -f 1)
 
 ◦•●◉✿ by Sã Śâjjãd ✿◉●•◦" -d "chat_id=$tg_id" -d "parse_mode=HTML"
