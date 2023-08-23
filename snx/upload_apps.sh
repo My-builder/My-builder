1b6d9c2797e4d60963b9d6dca3f1cf89cc32e7a3
Sa Sajjad <sqsa93103@gmail.com>
2023-08-23 23:40:39 +0600

    battery test vmro
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/upload_apps.sh
+++ snx/upload_apps.sh
@@ -6,10 +6,11 @@ cd ..
 cd Settings
 zip Settings.zip Settings.apk 
 bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh)
-# cd ..
-# cd ..
-# cd ..
-# cd framework
-# bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f *.apk
+cd ..
+cd ..
+cd ..
+cd framework
+zip framework-res.apk Android.zip
+bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh)
 # bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f framework.jar
     
