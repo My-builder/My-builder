b3157cca6f0ca743aa8272238f14ebcd2251b28d
Sa Sajjad <sqsa93103@gmail.com>
2023-08-23 22:18:16 +0600

    battery test vmro
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/notify.sh
+++ snx/notify.sh
@@ -1,11 +1,8 @@
 curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=
 ◦•●◉✿ 𝔹𝕦𝕚𝕝𝕕 𝕊𝕥𝕒𝕣𝕥𝕖𝕕 ✿◉●•◦
 
+➜Info~ <code>$rom_name/$CIRRUS_CHANGE_TITLE</code>
 
-Info~ <code>$rom_name/$CIRRUS_CHANGE_TITLE</code>
-      
-      
-Status~ <a href='https://cirrus-ci.com/build/$CIRRUS_BUILD_ID'>Click here</a>
-      
-      
+➜Status~ <a href='https://cirrus-ci.com/build/$CIRRUS_BUILD_ID'>Click here</a>
+
 ◦•●◉✿ by Sã Śâjjãd ✿◉●•◦ " -d "chat_id=$tg_id" -d "parse_mode=HTML"
