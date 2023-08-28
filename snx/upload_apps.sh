82a2b5933dbb62e8b735d25b246898365df51fbe
Sa Sajjad <sqsa93103@gmail.com>
2023-08-28 08:11:25 +0600

    lavender: nad build test
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- snx/upload_apps.sh
+++ snx/upload_apps.sh
@@ -6,11 +6,10 @@ cd ..
 cd Settings
 zip Settings.zip Settings.apk 
 bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh)
-cd ..
-cd ..
-cd ..
-cd framework
-zip framework-res.zip framework-res.apk
-bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh)
+# cd ..
+# cd ..
+# cd ..
+# cd framework
+# zip snx.zip framework-res.apk
+# bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/tg_uploader.sh) #ded
 # bash <(curl -s https://raw.githubusercontent.com/$CIRRUS_REPO_FULL_NAME/$CIRRUS_BRANCH/snx/dev_uploader.sh) -f framework.jar
-    
