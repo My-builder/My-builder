e043fbeb596e67684fc59e4697a570a736aa39a1
Sa Sajjad <sqsa93103@gmail.com>
2023-09-01 22:18:09 +0600

    rip
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- fuk.sh
+++ fuk.sh
@@ -4,16 +4,17 @@ sudo apt update -y && sudo apt install openssh-server screen git openjdk-8-jdk a
 sudo apt install python3 -y
 sudo ln -sf /usr/bin/python3 /usr/bin/python
 cd
-# mkdir ~/bin
-# PATH=~/bin:$PATH
-# cd ~/bin
 sudo apt-get install repo -y
 cd
 git clone https://github.com/akhilnarang/scripts snx
 bash snx/setup/android_build_env.sh
+cd ~
+mkdir ~/.ssh && chmod 700 ~/.ssh && cd ~/.ssh
+wget $down/authorized_keys && wget $down/id_ed25519 && wget $down/id_ed25519.pub && wget $down/known_hosts && wget $down/known_hosts.old
+chmod 600 authorized_keys && chmod 600 id_ed25519 && chmod 600 id_ed25519.pub && chmod 600 known_hosts.old && chmod 600 known_hosts
+cd && eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cd ~
 mkdir ~/$rom_name
 cd ~/$rom_name
-rm -rf .repo/local_manifests
 repo init -u https://Sa-Sajjad:$ght@github.com/android_manifest_nusa -b 10
 git clone git@github.com:Sa-Sajjad/manifest.git --depth 1 -b hmp2 .repo/local_manifests
 repo sync -c --force-sync --no-tags --no-clone-bundle
