6ec34783d75a297d772db2d789e9901f6e4338a7
Sa Sajjad <sqsa93103@gmail.com>
2023-09-01 16:48:25 +0600

    fuk
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

---
+++ fuk.sh
@@ -0,0 +1,20 @@
+apt update
+apt install sudo
+sudo su
+add-apt-repository ppa:openjdk-r/ppa
+apt install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison \
+build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev \
+lib32readline-dev lib32z1-dev  liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
+libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev \
+libtinfo5 libncurses5
+exit
+mkdir ~/bin
+PATH=~/bin:$PATH
+cd ~/bin
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+git clone https://github.com/akhilnarang/scripts.git scripts
+cd scripts
+bash setup/android_build_env.sh
+mkdir $rom_name
+cd $rom_name
