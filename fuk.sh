ec860c7858bbbff25b60691216c070ab3900c5e0
Sa Sajjad <sqsa93103@gmail.com>
2023-09-01 18:01:27 +0600

    Track our own fork(s) for 2023-08 ASB patching
    Signed-off-by: Sa Sajjad <sqsa93103@gmail.com>

--- fuk.sh
+++ fuk.sh
@@ -1,12 +1,7 @@
 apt update
 apt install sudo
 sudo su
-add-apt-repository ppa:openjdk-r/ppa
-apt install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison \
-build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev \
-lib32readline-dev lib32z1-dev  liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
-libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev \
-libtinfo5 libncurses5
+apt install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev libtinfo5 libncurses5
 exit
 mkdir ~/bin
 PATH=~/bin:$PATH
